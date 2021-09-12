222bbd78a2410b2d1516574d8e09c02c087dda70
Ankit Shaw <ankitshaw42@protonmail.com>
2021-09-12 21:12:56 +0530

    Add error handling middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,42 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { getUsers } = require('./services/user.service');
+const { userService } = require('./services/user.service');
+const { AppError } = require('./utils/errors');
 
 const app = express();
 
 app.use(cors({ origin: true }));
 
 app.get('/api/users', async (req, res) => {
-  const users = await getUsers();
+  const users = await userService.getUsers();
   res.json(users);
+});
+
+app.get('/api/users/:id', async (req, res, next) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Catch all middleware for unavailable routes
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 const port = process.env.PORT || 8000;
--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,17 +1,37 @@
 const { db } = require('../config/firestore');
 
-const getUsers = async () => {
-  const snapshot = await db.collection('users').get();
-
-  const users = [];
-
-  for (const doc of snapshot.docs) {
-    const { firstName, lastName } = doc.data();
-
-    users.push({ id: doc.id, firstName, lastName });
+class UserService {
+  async init() {
+    this.userRef = db.collection('users');
+    this.userSnapshot = await this.userRef.get();
   }
 
-  return users;
-};
+  async getUsers() {
+    const users = [];
 
-module.exports = { getUsers };
+    for (const doc of this.userSnapshot.docs) {
+      const { firstName, lastName } = doc.data();
+
+      users.push({ id: doc.id, firstName, lastName });
+    }
+
+    return users;
+  }
+
+  async getUserById(userId) {
+    const user = await this.userRef.doc(userId).get();
+    if (!user.exists) {
+      throw new Error('User not found.');
+    }
+
+    return user.data();
+  }
+}
+
+const userService = new UserService();
+
+(async function () {
+  await userService.init();
+})();
+
+module.exports = { userService };
