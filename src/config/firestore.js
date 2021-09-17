9193c1b174015645977d8511e015c265d1787d94
Ankit Shaw <ankitshaw42@protonmail.com>
2021-09-17 14:40:02 +0530

    Move config files to a single folder

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -3,7 +3,6 @@ const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.FIRESTORE_CREDS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL:
 });
 
 const db = admin.firestore();
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(process.cwd(), 'src', 'config', '.env'),
+});
 const express = require('express');
 const cors = require('cors');
 const { userService } = require('./services/user.service');
-const { AppError } = require('./utils/errors');
+const { AppError } = require('./utils/AppError');
+const handleErrors = require('./middlewares/errors');
 
 const app = express();
 
 app.use(cors({ origin: true }));
+app.use(express.json());
 
 app.get('/api/users', async (req, res) => {
   const users = await userService.getUsers();
@@ -17,7 +22,7 @@ app.get('/api/users/:id', async (req, res, next) => {
   try {
     const user = await userService.getUserById(req.params.id);
 
-    res.json(user);
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
@@ -29,15 +34,7 @@ app.all('*', (req, res, next) => {
 });
 
 // Error handler middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(handleErrors);
 
 const port = process.env.PORT || 8000;
 
